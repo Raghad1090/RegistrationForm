2336e300ef90dee81ba1b50bf80f3d43eeaac6e4
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-11-01 09:44:03 +0300

    edit hint color

--- app/src/main/java/com/example/registrationform/MainActivity.kt
+++ app/src/main/java/com/example/registrationform/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.registrationform
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.widget.Button
+import android.widget.EditText
 import com.example.registrationform.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        lateinit var binding: ActivityMainBinding
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,28 +23,41 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    }
-
-
 
     fun registration() {
 
-        var name = R.id.name
-        var email = R.id.email_address
-        var password = R.id.paassword
-        var re_password = R.id.password_renter
-        var birth_date = R.id.birthday_date
+        var name = findViewById(R.id.name) as EditText
+        var email_address = findViewById(R.id.email_address) as EditText
+        var password = findViewById(R.id.password) as EditText
+        var re_password = findViewById(R.id.password_renter) as EditText
+        var birth_date = findViewById(R.id.birthday_date) as EditText
 
 
-        if ((name && email) && (password && re_password) && birth_date = !empty) {
-
-
-        } if (password == re_password ){
-
+        if (name == checkNotNull(name)) {
 
         }
 
+        if (email_address == checkNotNull(email_address)) {
+
+        }
+        if (password == checkNotNull(password)) {
+
+        }
+        if (re_password == checkNotNull(re_password)) {
+
+        }
+        if (birth_date == checkNotNull(birth_date)) {
+
+        }
+        if (password == re_password) {
+
+        }
+
+
+
+    }
 }
+
 
 
 
